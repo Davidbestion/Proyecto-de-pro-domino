55f22e421ea089bed0ccec49156ab5c1e6c6637f
Davidbestion <104589703+Davidbestion@users.noreply.github.com>
2022-06-10 20:13:22 -0400

    Ya cree, de nuevo, la biblioteca y el form, oooooootra vez.......

---
+++ Clases del proyecto/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Clases_del_proyecto
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Interfaz Grafica/Form1.cs
@@ -0,0 +1,10 @@
+namespace Interfaz_Grafica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
